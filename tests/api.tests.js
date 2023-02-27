9abe9f89bf062a6d7941cf6c7cb51334fd030b0c
ZacBirgen <122633775+ZacBirgen@users.noreply.github.com>
2023-02-27 11:25:04 -0600

    Added test tests

--- tests/api.tests.js
+++ tests/api.tests.js
@@ -2,3 +2,19 @@ const fetch = require('node-fetch')
 
 const baseURL = 'http://localhost:3000'
 
+const _fetch = async (method, path, body) => {
+    body = typeof body === 'string' ? body : JSON.stringify(body)
+    const headers = { 'Content-Type': 'application/json' }
+    const res = await fetch(baseUrl + path, { method, body, headers })
+    if(res.status < 200 || res.status > 299)
+      throw new Error(`API returned status ${res.status}`)
+    return res.json()
+  }
+
+  describe('API tests', () => {
+
+    test('GET /api/calculator', async () => {
+      const vacations = await _fetch('get', '/api/vacations')
+      expect(vacations.json).not.toBe("High Risk")
+    })
+  })
