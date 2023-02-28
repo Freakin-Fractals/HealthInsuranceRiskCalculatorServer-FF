92f6db8f20ff6afa6ab3c15490d69164c9e84abd
ZacBirgen <isaacrbirgen@lewisu.edu>
2023-02-27 19:37:41 -0600

    Added comments

--- calculator.js
+++ calculator.js
@@ -10,15 +10,23 @@ const port = process.env.PORT || 3000
 app.get('/calculation', (request, response) => {
 	console.log('Calling "/add-two-integers" on the Node.js server.')
 	var inputs = url.parse(request.url, true).query
+  //Using this we can see the values that the site sent us, and their names
   console.log(inputs)
+
+  //Here are the variables; if need to add more follow examples
   let age = parseInt(inputs.age)
 	let height = parseInt(inputs.height)
 	let weight = parseInt(inputs.weight)
   let blood = inputs.blood
   let disease = parseInt(inputs.disease)
+
+  //Example of calucation; should be replaced with functions eventually
   let sum = height + weight
+
+  //We return our results as an object, namdes of value returned need to be comunicated
+  let results = {"sum": sum.toString(),"age":  age,"blood":  blood,"dis":  disease}
 	response.type('text/plain')
-	response.json(sum.toString())
+	response.json(results)
 })
 
 // custom 404 page
